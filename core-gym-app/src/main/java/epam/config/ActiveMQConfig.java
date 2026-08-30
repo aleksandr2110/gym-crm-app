1b286a40676e9982335aa846a303528cb0804fd8
aleksandr2110 <utel200519@gmail.com>
2026-08-30 11:41:31 +0300

    fixed docker and yml files

--- core-gym-app/src/main/java/epam/config/ActiveMQConfig.java
+++ core-gym-app/src/main/java/epam/config/ActiveMQConfig.java
@@ -1,10 +1,7 @@
 package epam.config;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
 @EnableJms
